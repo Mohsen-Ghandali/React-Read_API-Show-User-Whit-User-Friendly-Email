03205a8236ff9135a02c9f6b9f973b8e0b8a854d
Mohsen Ghandali <84251780+Mohsen-Ghandali@users.noreply.github.com>
2023-08-12 10:48:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './Components/App/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render (
   <React.StrictMode>
     <App />
   </React.StrictMode>
